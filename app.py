8f783fe7702ed3e0f4e43e3f968834984dc29833
eojin987 <eojin611613@gmail.com>
2026-05-27 15:54:33 +0000

    수정

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def handle_send_to_bot(data):
             
             emit('chat_update', {'sender': sender_name, 'message': msg_text}, broadcast=True)
     except ValueError:
-            emit('chat_update', {'sender': '주동자', 'message': '뭐라는 거야 ㅡㅡ 장난하냐? 똑바로 말해'}, broadcast=True)
+            emit('chat_update', {'sender': '주동자', 'message': '뭐라는 거야 ㅡㅡ'}, broadcast=True)
     except Exception as e:
         emit('chat_update', {'sender': '시스템', 'message': "오류가 발생했습니다: " + str(e)}, broadcast=True)
 
